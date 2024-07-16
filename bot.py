7ddc6829f784248527a629ad0928776cabe56181
xxaldebaran <darinkatovstogan@gmail.com>
2024-07-16 16:24:33 +0300

    Added answering and reply functionality.

--- bot.py
+++ bot.py
@@ -2,8 +2,13 @@ from aiogram import Bot, Dispatcher
 import asyncio
 from aiogram.types import Message 
 
-bot = Bot(token = '')
+bot = Bot(token = '7476823431:AAH8EcDuq-wlOGd-XdQ7eeiG2zzJXUQy7Zs')
 disp = Dispatcher()
+
+@disp.message()
+async def cmd_start(message: Message):
+    await message.answer("Hi there!")
+    await message.reply("What`s poping?")
 
 async def main():
     await disp.start_polling(bot)
