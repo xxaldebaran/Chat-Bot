9b6319be4933e01c17e84f5b412c9ed60ec87c54
xxaldebaran <darinkatovstogan@gmail.com>
2024-07-16 16:39:13 +0300

    Added basic dispatchers.

--- bot.py
+++ bot.py
@@ -1,17 +1,29 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 import asyncio
 from aiogram.types import Message 
+from aiogram.filters import CommandStart, Command 
 
 bot = Bot(token = '7476823431:AAH8EcDuq-wlOGd-XdQ7eeiG2zzJXUQy7Zs')
 disp = Dispatcher()
 
-@disp.message()
+@disp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer("Hi there!")
     await message.reply("What`s poping?")
+
+@disp.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.answer("How can I help you?")
+
+@disp.message(F.text == "Tell a dad`s joke!")
+async def dad_joke(message: Message):
+    await message.answer("Do you know why you never see elephants hiding in trees? It`s because they`re so good at it.")
 
 async def main():
     await disp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Bot is down.")
